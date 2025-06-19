05e33011a7566cf8db2f2231f55de95eaa304938
DaniloSV07 <danilosouzavoiskie@gmail.com>
2025-06-19 16:56:12 -0300

    Update app version to 1.0.18, enable cleartext traffic in app.json, and modify admin layout to hide tab bar on additional routes for improved navigation experience.

--- src/app/(logged)/admin/_layout.tsx
+++ src/app/(logged)/admin/_layout.tsx
@@ -5,7 +5,10 @@ export default function LoggedTabsLayout() {
   const pathname = usePathname();
   const hideTabBar =
     pathname.startsWith("/admin/teste/") ||
-    pathname.startsWith("/admin/profile/");
+    pathname.startsWith("/admin/profile/") ||
+    pathname.startsWith("/admin/home/") ||
+    pathname.startsWith("/admin/members/");
+
   return (
     <Tabs
       tabBar={(props) => !hideTabBar && <CustomTabBar />}
