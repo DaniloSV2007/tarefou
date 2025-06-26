d63287060a2278812d353f0eff89513a001c0a86
DaniloSV07 <danilosouzavoiskie@gmail.com>
2025-06-25 23:03:49 -0300

    Integrate theme support in Logged component and render a background view. This update enhances the user interface by applying the app's theme colors to the main view.

--- src/app/(logged)/index.tsx
+++ src/app/(logged)/index.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from "@/context/AuthContext";
+import { useAppTheme } from "@/hooks/useAppTheme";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import { useEffect } from "react";
@@ -7,6 +8,7 @@ import { Text, View } from "react-native";
 export default function Logged() {
   const { logout } = useAuth();
   const router = useRouter();
+  const theme = useAppTheme();
 
   const getRole = async () => {
     const role = await AsyncStorage.getItem("userRole");
@@ -25,5 +27,7 @@ export default function Logged() {
     getRole();
   }, []);
 
-  return null;
+  return (
+    <View style={{ backgroundColor: theme.colors.background, flex: 1 }}></View>
+  );
 }
