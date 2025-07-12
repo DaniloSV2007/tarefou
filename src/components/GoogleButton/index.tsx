26b8f05c7bf8b5737c0a2bdc5b09c13abdeec368
DaniloSV07 <danilosouzavoiskie@gmail.com>
2025-07-12 14:41:15 -0300

    Update app version to 1.1.2, add expo-web-browser and expo-auth-session dependencies, and ensure compatibility with existing components. Refactor package-lock.json to reflect updated package versions and improve overall dependency management.

--- src/components/GoogleButton/index.tsx
+++ src/components/GoogleButton/index.tsx
@@ -11,53 +11,19 @@ import {
 import { Button, TouchableRipple } from "react-native-paper";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-// import * as WebBrowser from "expo-web-browser";
-// import * as Google from "expo-auth-session/providers/google";
-import { auth } from "../../../FirebaseConfig";
-import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
-import Constants from "expo-constants";
 
 interface GoogleButtonProps {
   onPress: () => void;
 }
-
-// WebBrowser.maybeCompleteAuthSession();
 
 export default function GoogleButton({ onPress }: GoogleButtonProps) {
   const theme = useAppTheme();
   const { isDark } = useThemeContext();
   const { t } = useTranslation();
 
-  // const [request, response, promptAsync] = Google.useAuthRequest({
-  //   clientId:
-  //     "543165319921-hsshsqs72k3autofjas1jvc1to24fkon.apps.googleusercontent.com",
-  //   iosClientId:
-  //     "543165319921-hsshsqs72k3autofjas1jvc1to24fkon.apps.googleusercontent.com",
-  //   androidClientId:
-  //     "543165319921-hsshsqs72k3autofjas1jvc1to24fkon.apps.googleusercontent.com",
-  //   webClientId:
-  //     "543165319921-hsshsqs72k3autofjas1jvc1to24fkon.apps.googleusercontent.com",
-  // });
-
-  // useEffect(() => {
-  //   if (response?.type === "success") {
-  //     const { idToken } = response.authentication!;
-  //     const credential = GoogleAuthProvider.credential(idToken);
-  //     signInWithCredential(auth, credential)
-  //       .then(() => {
-  //         console.log("Usuário autenticado com Google");
-  //       })
-  //       .catch((error) => {
-  //         console.error("Erro ao autenticar:", error);
-  //       });
-  //   }
-  // }, [response]);
-
   return (
     <View style={{ borderRadius: 32, overflow: "hidden" }}>
       <Pressable
-        disabled
-        // onPress={() => promptAsync()}
         style={[
           styles.button,
           { backgroundColor: theme.custom.cardTaskBackground },
